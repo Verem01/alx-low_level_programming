69cecacf5e995d790e3aab1ac984b98086b1cda7
Terhemba-Uta Verem <terhembaverem3@gmail.com>
2022-07-07 18:34:07 -0700

    completed advanced task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,25 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
+
 /**
- * main - main block
- * Description: computes and prints the sum of all the multiples of 3 or
- * 5 below 1024 (excluded), followed by a new line
- * Return: 0
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
-int main(void)
+void print_number(int n)
 {
-	int c = 0;
-	int sum = 0;
+	unsigned int m, d, count;
 
-	while (c < 1024)
+	if (n < 0)
 	{
-		if (c % 3 == 0 || c % 5 == 0)
-		{
-			sum += c;
-		}
-
-		c++;
+		_putchar(45);
+		m = n * -1;
 	}
-	printf("%i\n", sum);
-	return (0);
+	else
+	{
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
