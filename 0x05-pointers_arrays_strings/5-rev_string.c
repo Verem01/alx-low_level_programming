b8eb53374d11148f7fdd79b64d386c7ffc407b35
Terhemba-Uta Verem <terhembaverem3@gmail.com>
2022-07-12 15:03:29 -0700

    editted file 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_rev - prints reversed string, followed by a new line
+ * rev_string - prints reversed string, followed by a new line
  * @s: pointer to the string to print
  * Return: void
 */
