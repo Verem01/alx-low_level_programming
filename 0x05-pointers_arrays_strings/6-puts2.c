21efc0407b98334b003a5040059950342c2d9498
Terhemba-Uta Verem <terhembaverem3@gmail.com>
2022-07-12 15:15:36 -0700

    completed from 6 to 9

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* puts2 - print one char out of 2 of a string
+*@str: string to print
+*Return: Nothing
+*/
+void puts2(char *str)
+{
+	int length;
+	char l;
+
+	for (length = 0; *(str + length) != 0; length++)
+	{
+		if (length % 2 == 0)
+		{
+			l = *(str + len);
+			_putchar(l);
+		}
+	}
+	_putchar('\n');
+}
